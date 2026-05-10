9c914669720fd618d8af7de9f5fb56ebf60f00f9
Franco Crespo <francoscrespo@gmail.com>
2026-05-10 00:57:35 -0300

    Clase 6: Introducción a JavaScript, manejo de variables

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+let usuario = prompt("¡Hola! ¿Cuál es tu nombre?");
+
+if (usuario === "" || usuario === null) {
+    usuario = "Invitado";
+}
+
+const mensajeElemento = document.getElementById('mensaje-saludo');
+
+
+let fecha = new Date();
+let hora = fecha.getHours();
+let saludo;
+
+if (hora < 12) {
+    saludo = "¡Buenos días, " + usuario + "! ";
+} else if (hora < 20) {
+    saludo = "¡Buenas tardes, " + usuario + "! ";
+} else {
+    saludo = "¡Buenas noches, " + usuario + "! ";
+}
+
+
+mensajeElemento.textContent = saludo;
+
+
+function marcarFavorito(elemento) {
+    elemento.textContent = "¡Favorita!";
+    elemento.style.backgroundColor = "#e23636";
+    console.log("Película marcada por: " + usuario);
+}
